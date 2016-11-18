a6f276131e365c17c845fdacc31626f5a20416d5
jessekorzan <jesse@emptycan.com>
2016-11-18 11:19:25 -0800

    server-side check demo

--- assets/js/jk.rUhuman.js
+++ assets/js/jk.rUhuman.js
@@ -2,6 +2,7 @@ var rUhuman = (function ($) {
     // 2016
     // @jessekorzan
     // www.emptycan.com
+    // https://github.com/jessekorzan/rUhuman
     //
     // SIMPLE CAPTCHA
     // Are you human or bot or what?
@@ -20,7 +21,8 @@ var rUhuman = (function ($) {
     		// captcha box appended to form
             var _box = '<div class="' + this.config.classRef + '">';
                 _box += '<label><span></span><span></span><i></i>';
-                _box += '<input type="range" max="12" min="2" value="1" required>';
+                _box += '<input name="rUhuman_chk" type="range" max="12" min="2" value="1" required>';
+                _box += '<input name="rUhuman_dbl_chk" type="text" style="display: none;" value="bot" required>';
                 _box += '</label></div>';
                             
             $(this.config.form).find(this.config.submit).before(_box);
@@ -31,7 +33,8 @@ var rUhuman = (function ($) {
     		var _this = this,
     		    _box = $("." + this.config.classRef),
     		    _mathElms = _box.find("label span"),
-    		    _userInput = _box.find("input"),
+    		    _userInput = _box.find("input[name=rUhuman_chk]"),
+    		    _hiddenInput = _box.find("input[name=rUhuman_dbl_chk]"),
     		    _form = _box.closest("form"),
     		    _sliderOutput = _box.find("i");
     		    _ANSWER = 0;
@@ -45,8 +48,9 @@ var rUhuman = (function ($) {
                 $(this).html(_rnd);
             });
             
+            // rUhuman?!
+            // client side check for bot
             setInterval(function(){
-                // keep checking for slider value
                 var _chk = Number(_userInput.val());
                 _sliderOutput.html(_chk);
                 if (_chk == Number(_ANSWER)) {
@@ -57,6 +61,16 @@ var rUhuman = (function ($) {
                     _box.removeClass("confirmed");
                 }
             }, 100);
+            
+            // rUhuman?! rUhuman?!
+            // hidden input to match value (i.e. server-side dbl check to catch the really tricky bots)
+            // OPTIONAL STEP see example in repo
+            _userInput.on("mousedown", this, function(){
+                _hiddenInput.val(_ANSWER);
+            });
+            _userInput.on("touchstart", this, function(){
+                _hiddenInput.val(_ANSWER);
+            });
         },
         init :  function () {
             // set us up the bomb
